3ce284654ca1f147401e722b4428c03b726c054c
AbbDada <49881729+MouAbba@users.noreply.github.com>
2020-12-28 04:58:38 -0800

    Add files via upload

---
+++ Tp2_WCF_Linq_Sql/WcfServiceEnseignement/IServiceEnseignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceEnseignement
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceEnseignment" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceEnseignment
+    {
+        [OperationContract]
+        List<Eleve> getByCNE(string s);
+        void DoWork();
+    }
+}
---
+++ Tp2_WCF_Linq_Sql/WcfServiceEnseignement/ServiceEnseignment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceEnseignement
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceEnseignment" in both code and config file together.
+    public class ServiceEnseignment : IServiceEnseignment
+    {
+        DataClassesEnseignmentDataContext l = new DataClassesEnseignmentDataContext();
+        public List<Eleve> getByCNE(string s)
+        {
+            return (from p in l.Eleves where p.CNE == s select p).ToList();
+        }
+        public void DoWork()
+        {
+        }
+    }
+}
